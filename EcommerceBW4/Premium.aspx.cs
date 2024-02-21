5714f5a7da16f3436589da0e0dfb1faa243efcd3
D'Amico Salvatore Alessandro <122369512+Frelser77@users.noreply.github.com>
2024-02-21 10:58:24 +0100

    WIP-Update Premium page add-html remove aspcontent

--- EcommerceBW4/Premium.aspx.cs
+++ EcommerceBW4/Premium.aspx.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace EcommerceBW4
 {
@@ -12,6 +7,13 @@ namespace EcommerceBW4
         protected void Page_Load(object sender, EventArgs e)
         {
 
+
+        }
+
+        protected void Search_Click(object sender, EventArgs e)
+        {
+            string searchText = searchInput.Value.Trim(); // Ottieni il testo di ricerca dall'input
+            Response.Redirect($"Default.aspx?search={searchText}");
         }
     }
 }
